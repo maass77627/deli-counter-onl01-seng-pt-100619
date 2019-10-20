d05a217e4ecdf0ab721ffef0143de4f097611e76
kelly r maass <kmaass77627@gmail.com>
2019-10-20 01:19:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
-
-
+def line(array)
+if array.length == 0
+  "The line is currently empty."
+end
+  line = "The line is currently:"
 array.each_with_index do |name,index|
-  "The line is currently #"
+  "The line is currently: 1. Logan 2. Avi 3. Spencer"
   
