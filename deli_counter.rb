01cd67a8b1c9df772ca07d0ed5f072cf1b9b5061
kelly r maass <kmaass77627@gmail.com>
2019-10-20 01:41:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(array)
-  customers = "The line is currently:"
 if array.length == 0
   "The line is currently empty."
 end
+puts "The line is currently: "
   
-array.each_with_index do |name,index|
+array.each_with_index do |name, index|
  
   
