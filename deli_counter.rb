479529b6f021a81af696d034a97fc8739b44b62c
kelly r maass <kmaass77627@gmail.com>
2019-10-20 02:29:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 
 def line(array)
 if array.length == 0
-  puts "The line is currently empty."
+  return "The line is currently empty."
 end
 puts "The line is currently: "
   array.each_with_index do |name, index|
